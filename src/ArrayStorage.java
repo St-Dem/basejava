dcb459025b48c6f89bfc3a5fcf7aaf2e11cd59e5
Stanislav Demidov <dmd.stanislav@gmail.com>
2020-10-23 01:22:47 +0300

    Заливка кода тестового задания

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,67 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int size = this.size();
+       for (int i = 0; i < size; i ++){
+           storage[i] = null;
+            }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
+
     }
 
     Resume get(String uuid) {
-        return null;
+        for (int i = 0; i < this.size(); i++) {
+            if (uuid != null && storage[i].toString().equals(uuid)) {
+                return storage[i];
+            }
+        }
+        Resume resume = new Resume();
+        resume.uuid = uuid;
+        return resume;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < this.size(); i++) {
+            if (storage[i].toString().equals(uuid)) {
+                storage[i] = null;
+                break;
+            }
+        }
+        for (int j = 0; j < this.size(); j++) {
+            if (storage[j] == null && storage[j + 1] != null) {
+                storage[j] = storage[j + 1];
+                storage[j + 1] = null;
+            }
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resume = new Resume[this.size()];
+        for (int i = 0; i < this.size(); i++) {
+            if (storage[i] != null) resume[i] = storage[i];
+        }
+        return resume;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                count++;
+            }
+        }
+        return count;
     }
 }
