c81c13afc1cb70827c969f03a89e901441f54b74
Stanislav Demidov <dmd.stanislav@gmail.com>
2020-10-23 07:20:00 +0300

    Финальная прака - удаление возможности ввести null  в Resume.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,27 +3,31 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-/*
-*Удаляем все значимые части массива
- */
+
+    /*
+     *Удаляем все значимые части массива
+     */
     void clear() {
         int size = this.size();
-       for (int i = 0; i < size; i ++){
-           storage[i] = null;
-            }
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
     }
-/*
-* Добавляем значения в массив
- */
+
+    /*
+     * Добавляем значения в массив
+     */
     void save(Resume r) {
-      storage[this.size()] = r;
-
+        if (r.toString() != null) {
+            storage[this.size()] = r;
+        }
 
     }
-/*
-*Получить объект по ключу
-* Если объекта нет выводится ключ
- */
+
+    /*
+     *Получить объект по ключу
+     * Если объекта нет выводится ключ
+     */
     Resume get(String uuid) {
         for (int i = 0; i < this.size(); i++) {
             if (uuid != null && storage[i].toString().equals(uuid)) {
@@ -34,9 +38,10 @@ public class ArrayStorage {
         resume.uuid = uuid;
         return resume;
     }
-/*
-* Удаляется объект по ключу
- */
+
+    /*
+     * Удаляется объект по ключу
+     */
     void delete(String uuid) {
         for (int i = 0; i < this.size(); i++) {
             if (storage[i].toString().equals(uuid)) {
@@ -57,7 +62,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     /*
-    * Возвраащается массив состоящий только из значений (не null)
+     * Возвраащается массив состоящий только из значений (не null)
      */
     Resume[] getAll() {
         Resume[] resume = new Resume[this.size()];
@@ -66,9 +71,10 @@ public class ArrayStorage {
         }
         return resume;
     }
-/*
-* Подсчет элементов массива
- */
+
+    /*
+     * Подсчет элементов массива
+     */
     int size() {
         int count = 0;
         for (Resume resume : storage) {
