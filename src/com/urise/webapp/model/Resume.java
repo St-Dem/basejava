8c30234d9983508d90c20da3f9bdb8948b0dd180
Stanislav Demidov <dmd.stanislav@gmail.com>
2020-11-04 13:54:51 +0300

    Исправлены наименования методов, после if() всегда идет {}.

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -6,20 +6,10 @@ package com.urise.webapp.model;
 public class Resume {
 
     // Unique identifier
-   private String uuid;
+    String uuid;
 
     @Override
     public String toString() {
         return uuid;
     }
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-
 }
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,134 +1,32 @@
 package com.urise.webapp.storage;
 
-import com.urise.webapp.model.Resume;
-
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
-    private int size;
+    Resume[] storage = new Resume[10000];
 
-    /*
-     *Удаляем все значимые части массива
-     */
-    public void clear() {
-        storage = Arrays.copyOfRange(storage, 0, 0);
-        size = 0;
+    void clear() {
     }
 
-    /*
-     * Не понимаю что мы должны менять.
-     * Если uuid то нужно сравниваться по другому ключу, а ключей больше нет.
-     */
-    public void update(Resume resume) {
-        if (resume == null || size == 0) return;
-        int j = -1;
-        try {
-            for (int i = 0; i < size; i++) {
-                if (storage[i].getUuid().equals(resume.getUuid())) {
-                    storage[i].setUuid(resume.getUuid());
-                    j = i;
-                    break;
-                }
-            }
-            if (j == -1) System.out.println("Резюме не существует");
-        } catch (Exception e) {
-
-        }
+    void save(Resume r) {
     }
 
-    /*
-     * Добавляем значения в массив
-     * Если резюме отсутствует выходим.
-     * Если резюме присутствует в базе выходим.
-     * Если массив резюме полон - выводится предупреждение.
-     */
-    public void save(Resume r) {
-        if (r.getUuid().isEmpty()) return;
-        if (size == 9999) {
-            System.out.println("Резюме заполнено");
-            return;
-        }
-        try {
-            for (Resume a : storage) {
-                if (r.getUuid().equals(a.getUuid())) {
-                    System.out.println("Резюме существует");
-                    return;
-                }
-            }
-
-        } catch (Exception e) {
-
-        }
-        storage[size] = r;
-        size++;
-
-
-    }
-
-    /*
-     *Получить объект по ключу
-     * Если объекта нет выводится null
-     * Если ничего не введено возвращается null
-     */
-    public Resume get(String uuid) {
-        if (uuid == null) return null;
-        for (int i = 0; i < size; i++) {
-            if (storage[i].toString().equals(uuid)) {
-                return storage[i];
-            }
-        }
-        System.out.println("Резюме не существует");
+    Resume get(String uuid) {
         return null;
     }
 
-    /*
-     * Удаляется объект по ключу
-     * Если удалять нечего или неоткуда - ничего не делаем.
-     */
-    public void delete(String uuid) {
-        if (uuid == null || size == 0) return;
-        int j = -1;
-        for (int i = 0; i < size; i++) {
-            if (storage[i].toString().equals(uuid)) {
-                storage[i] = null;
-                j = i;
-                break;
-            }
-        }
-        if (j != -1) {
-            for (; j < size - 1; j++) {
-                storage[j] = storage[j + 1];
-            }
-            storage[size - 1] = null;
-            size--;
-        } else {
-            System.out.println("Резюме не существует");
-        }
-
+    void delete(String uuid) {
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    /*
-     * Возвращается массив состоящий только из значений (не null)
-     */
-    public Resume[] getAll() {
-        Resume[] resume = Arrays.copyOf(storage, size);
-
-        return resume;
+    Resume[] getAll() {
+        return new Resume[0];
     }
 
-    /*
-     * Возвращается количество ненулевых элементов массива
-     */
-    public int size() {
-        return size;
+    int size() {
+        return 0;
     }
-
-
 }
