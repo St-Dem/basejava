42fd0146029c062c7a7a71527db8fecd425b33b4
Stanislav Demidov <dmd.stanislav@gmail.com>
2020-11-10 20:28:00 +0300

    Вызов toString через Reflection

---
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,21 @@
+package com.urise.webapp.storage;
+
+import org.junit.Test;
+
+public class ArrayStorageTest extends AbstractArrayStorageTest{
+    public ArrayStorageTest(Storage storage) {
+        super(storage);
+    }
+
+    @Test
+    public void fillDeletedElement() {
+    }
+
+    @Test
+    public void insertElement() {
+    }
+
+    @Test
+    public void getIndex() {
+    }
+}
