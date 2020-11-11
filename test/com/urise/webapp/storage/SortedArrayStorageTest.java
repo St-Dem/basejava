337c1c4dd6c2dc519cfd987c5ae2077092763f53
Stanislav Demidov <dmd.stanislav@gmail.com>
2020-11-11 20:17:58 +0300

    Вызов toString через Reflection

---
+++ test/com/urise/webapp/storage/SortedArrayStorageTest.java
@@ -0,0 +1,10 @@
+package com.urise.webapp.storage;
+
+public class SortedArrayStorageTest extends AbstractArrayStorageTest{
+
+    public SortedArrayStorageTest() {
+        super(new SortedArrayStorage());
+    }
+
+
+}
