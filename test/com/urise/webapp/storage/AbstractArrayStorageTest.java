2345013ca7174414ca0029d3dcc42132f98fb8c5
Stanislav Demidov <dmd.stanislav@gmail.com>
2020-11-19 16:50:59 +0300

    Первая часть 5 урока.

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -8,11 +8,10 @@ import org.junit.Test;
 import java.util.UUID;
 
 public abstract class AbstractArrayStorageTest extends AbstractStorageTest {
-    private Storage storage;
+
 
     protected AbstractArrayStorageTest(Storage storage) {
         super(storage);
-        this.storage = storage;
     }
 
     @Test(expected = StorageException.class)
