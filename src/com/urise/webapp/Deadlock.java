fa87410e0c2d8dc68eb736319288136b86b02735
Stanislav Demidov <dmd.stanislav@gmail.com>
2020-12-10 14:44:37 +0300

    Task 11

--- src/com/urise/webapp/Deadlock.java
+++ src/com/urise/webapp/Deadlock.java
@@ -1,38 +1,32 @@
 package com.urise.webapp;
 
 public class Deadlock {
-    static Thread thread1 = new Thread(new Deadlock1());
-    static Thread thread2 = new Thread(new Deadlock2());
-
     public static void main(String[] args) {
-        thread1.start();
-        thread2.start();
+        int one = 1;
+        String two = "Russia";
+        deadlock(one, two);
+        deadlock(two, one);
     }
 
-    public static class Deadlock1 implements Runnable {
-
-        @Override
-        public void run() {
-            try {
-                thread2.join();
-                System.out.println("Deadlock1 is finished");
-            } catch (InterruptedException e) {
-                System.out.println("Deadlock1 was interapted");
-            }
-
-        }
-    }
-
-    public static class Deadlock2 implements Runnable {
-
-        @Override
-        public void run() {
-            try {
-                thread1.join();
-                System.out.println("Deadlock2 is finished");
-            } catch (InterruptedException e) {
-                System.out.println("Deadlock2 was interapted");
+    public static void deadlock(Object one, Object two) {
+        new Thread(() -> {
+            synchronized (one) {
+                System.out.println("Before deadlcok");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (two) {
+                    System.out.println("After deadlock");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
+        ).start();
     }
 }
