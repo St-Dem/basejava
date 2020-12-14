d79c59ab7bbaa909df2ef81ac33007b16d093d33
Stanislav Demidov <dmd.stanislav@gmail.com>
2020-12-14 14:11:51 +0300

    Task 12

--- src/com/urise/webapp/MainSteams.java
+++ src/com/urise/webapp/MainSteams.java
@@ -2,25 +2,40 @@ package com.urise.webapp;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class MainSteams {
-    private static Stream<Integer> stream;
-
     public static void main(String[] args) {
         System.out.println("Now minValue");
-        printArray(1, 2, 3, 3, 2, 3);
-        printArray(9, 8);
-        printArray();
+        int[] mas1 = new int[]{1, 2, 3, 3, 2, 3};
+        int[] mas2 = new int[]{9, 8};
+        int[] mas3 = new int[10];
+
+        printArray(mas1);
+        printArray(mas2);
+        printArray(mas3);
         System.out.println();
 
         System.out.println("Now oddOrEven");
-        printList(1, 2, 3, 3, 2, 3);
-        printList(8, 9);
-        printList(3, 4, 7, 2, 1);
-        printList();
-        printList(3, 4, 2, 1);
+        List<Integer> arr1 = getArraylist(1, 2, 3, 3, 2, 3);
+        List<Integer> arr2 = getArraylist(9, 8);
+        List<Integer> arr3 = getArraylist(5, 2, 3, 8, 3);
+        List<Integer> arr4 = getArraylist();
+
+        arr1 = oddOrEven(arr1);
+        arr2 = oddOrEven(arr2);
+        arr3 = oddOrEven(arr3);
+        arr4 = oddOrEven(arr4);
+
+        printList(arr1);
+        printList(arr2);
+        printList(arr3);
+        printList(arr4);
+    }
+
+    interface Filter<T> {
+        Predicate<? super java.lang.Integer> filtration();
     }
 
     private static int minValue(int[] values) {
@@ -30,22 +45,22 @@ public class MainSteams {
                 .reduce(0, (a, b) -> (a * 10) + b);
     }
 
-    private static void printArray(int... values) {
+    private static void printArray(int[] values) {
         System.out.println(minValue(values));
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        stream = integers.stream()
-                .parallel();
-
         return integers.stream()
                 .parallel()
                 .reduce(0, Integer::sum) % 2 == 1
-                ? stream
-                .filter(x -> x % 2 == 0)
-                .collect(Collectors.toList())
-                : stream
-                .filter(x -> x % 2 == 1)
+                ? getFilteringList(integers, () -> x -> x % 2 == 0)
+                : getFilteringList(integers, () -> x -> x % 2 == 1);
+    }
+
+    private static List<Integer> getFilteringList(List<Integer> integers, Filter<Integer> filter) {
+        return integers.stream()
+                .parallel()
+                .filter(filter.filtration())
                 .collect(Collectors.toList());
     }
 
@@ -53,8 +68,8 @@ public class MainSteams {
         return Arrays.asList(integers);
     }
 
-    private static void printList(Integer... integers) {
-        for (Integer r : oddOrEven(getArraylist(integers))) {
+    private static void printList(List<Integer> integers) {
+        for (Integer r : integers) {
             System.out.print(r + " ");
         }
         System.out.println();
