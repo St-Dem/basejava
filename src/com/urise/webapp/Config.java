d6c4857c1cf723b33205e9ceb5912b3b00f6ed26
Stanislav Demidov <dmd.stanislav@gmail.com>
2020-12-19 19:42:11 +0300

    Task 13

--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -4,22 +4,29 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Properties;
 
 public class Config {
+    private static final File PROPS = new File("config\\resumes.properties");
     private static final Config INSTANCE = new Config();
-    private static final File PROPS = new File(".config/resume.properties");
 
-    public Config getInstance() {
+    private Properties props = new Properties();
+    private File storageDir;
+
+    public static Config get() {
         return INSTANCE;
     }
 
     private Config() {
         try (InputStream is = new FileInputStream(PROPS)) {
-
-
+            props.load(is);
+            storageDir = new File(props.getProperty("storage.dir"));
         } catch (IOException e) {
-          throw  new IllegalStateException("pnvalid config file" + PROPS.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
         }
+    }
 
+    public File getStorageDir() {
+        return storageDir;
     }
 }
